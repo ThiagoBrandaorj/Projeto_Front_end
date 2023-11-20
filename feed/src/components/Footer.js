0b80bce1599717d7f0907051bf47687add626e42
BrenoFrança <brenofafrj3@gmail.com>
2023-11-20 00:03:42 -0300

    alterações header e footer

--- feed/src/components/Footer.js
+++ feed/src/components/Footer.js
@@ -1,22 +1,24 @@
-import { FaHome, FaStar, FaMapMarkerAlt } from "react-icons/fa";
-import './styles/Footer.css';
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+  background-color: #6990F4;
+  color: white;
+  padding: 10px;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+`;
 
 const Footer = () => {
-    return(
-    <footer>
-        <ul className="icons_list">
-            <li>
-                <FaHome/>
-            </li>
-            <li>
-                <FaStar/>
-            </li>
-            <li>
-                <FaMapMarkerAlt/>
-            </li>
-        </ul>
-    </footer>
-    )
+  return (
+    <StyledFooter>
+        Contato: 000-000/passeiocarioca@email.com
+    
+      &copy; 2023 Meu Site
+    </StyledFooter>
+  );
 };
 
 export default Footer;
--- feed/src/components/Header.js
+++ feed/src/components/Header.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import './styles/Header.css'; 
+import styled from 'styled-components';
+
+const StyledHeader = styled.h1`
+  font-family: 'Irish Grover', sans-serif;
+  color: white; 
+  margin: 0; 
+`;
+
+const HeaderContainer = styled.div`
+  background-color: #6990F4; 
+  padding: 10px; 
+  top: 0; 
+  z-index: 1000; 
+  align-items: center; /* Centraliza verticalmente os itens dentro do contêiner */
+  justify-content: center; /* Centraliza horizontalmente os itens dentro do contêiner */
+`;
 
 const Header = () => {
   return (
-    <div className="header-container">
-      <h1>Meu Cabeçalho</h1>   
-    </div>
+    <HeaderContainer>
+      <StyledHeader>Passeio Carioca</StyledHeader>
+    </HeaderContainer>
   );
 };
-
 
 export default Header;
